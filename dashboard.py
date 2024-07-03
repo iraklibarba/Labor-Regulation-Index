65cf1a90f6a1bbc10bda3e5f53ca635bed484600
iraklibarba <iraklibarbakadzeur@gmail.com>
2024-07-04 00:42:13 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -77,12 +77,29 @@ def update_graphs(selected_indicator, selected_year, selected_country):
     year_data = data[data['year'] == selected_year]
     
     # Create time series graphs for each indicator
-    lri_fig = px.line(filtered_data, x='year', y='lri', title='LRI Over Time')
-    employment_forms_fig = px.line(filtered_data, x='year', y='employment_forms', title='Employment Forms Over Time')
-    working_time_fig = px.line(filtered_data, x='year', y='working_time', title='Working Time Over Time')
-    dismissal_fig = px.line(filtered_data, x='year', y='dismissal', title='Dismissal Over Time')
+    lri_fig = px.line(filtered_data, x='year', y='lri', title='Labour Regulation Index Over Time')
+    lri_fig.update_xaxes(title_text="")
+    lri_fig.update_yaxes(title_text="", range=[-0.05, 1.05])
+    
+    employment_forms_fig = px.line(filtered_data, x='year', y='employment_forms', title='Different Forms of Employment Over Time')
+    employment_forms_fig.update_xaxes(title_text="")
+    employment_forms_fig.update_yaxes(title_text="", range=[-0.05, 1.05])
+    
+    working_time_fig = px.line(filtered_data, x='year', y='working_time', title='Regulation of Working Time Over Time')
+    working_time_fig.update_xaxes(title_text="")
+    working_time_fig.update_yaxes(title_text="", range=[-0.05, 1.05])
+    
+    dismissal_fig = px.line(filtered_data, x='year', y='dismissal', title='Regulation of Dismissal Over Time')
+    dismissal_fig.update_xaxes(title_text="")
+    dismissal_fig.update_yaxes(title_text="", range=[-0.05, 1.05])
+    
     employee_representation_fig = px.line(filtered_data, x='year', y='employee_representation', title='Employee Representation Over Time')
+    employee_representation_fig.update_xaxes(title_text="")
+    employee_representation_fig.update_yaxes(title_text="", range=[-0.05, 1.05])
+    
     industrial_action_fig = px.line(filtered_data, x='year', y='industrial_action', title='Industrial Action Over Time')
+    industrial_action_fig.update_xaxes(title_text="")
+    industrial_action_fig.update_yaxes(title_text="", range=[-0.05, 1.05])
     
     # Create choropleth map for the selected indicator and year
     choropleth_fig = px.choropleth(
@@ -102,7 +119,7 @@ def update_graphs(selected_indicator, selected_year, selected_country):
             xanchor='center',
             x=0.5,
             title=None,
-            len=0.3,  # Make the legend smaller
+            len=0.4,  # Make the legend smaller
         )
     )
     
@@ -111,7 +128,6 @@ def update_graphs(selected_indicator, selected_year, selected_country):
 @app.callback(
     Output('download-dataframe-csv', 'data'),
     [Input('btn-download', 'n_clicks')],
-    [Input('country-dropdown', 'value')],
     prevent_initial_call=True
 )
 def download_filtered_data(n_clicks, selected_country):
